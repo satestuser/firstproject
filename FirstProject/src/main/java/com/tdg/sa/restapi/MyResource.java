5071eff99f3f6ae0925094372f9c782f77d1eeb7
satestuser <SanketD@thedigitalgroup.com>
2017-02-16 18:20:49 +0530

    updated msg to test docker again

--- FirstProject/src/main/java/com/tdg/sa/restapi/MyResource.java
+++ FirstProject/src/main/java/com/tdg/sa/restapi/MyResource.java
@@ -20,7 +20,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-        return "Roger that! Got your request.";
+        return "Got it! Acknowledging back.";
     }
     
     

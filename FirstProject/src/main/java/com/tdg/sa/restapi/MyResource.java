fa504cf4858012655d729c6f2d7ae7dbd9658689
satestuser <SanketD@thedigitalgroup.com>
2017-03-01 15:34:24 +0530

    updated the message at 3.34pm

--- FirstProject/src/main/java/com/tdg/sa/restapi/MyResource.java
+++ FirstProject/src/main/java/com/tdg/sa/restapi/MyResource.java
@@ -20,7 +20,7 @@ public class MyResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getIt() {
-    	return "New message. Testing on 02/27/2017. Test at 5.55pm";
+    	return "New message. Testing on 03/01/2017. Test at 3.33pm";
     }
     
     
